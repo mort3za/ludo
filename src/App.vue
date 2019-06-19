c47bb644faa70845b6fe0d85fc3128a06884867f
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-06-19 10:14:31 +0430

    disable unused bootstrap modules, minor style app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <style lang="scss">
 @import "@/styles/global.scss";
+
+html,
+body,
+#app {
+  min-height: 100vh;
+}
+
+#app {
+  border-top: rem(4px) solid $brand-6;
+  border-bottom: rem(4px) solid $brand-6;
+}
 </style>
 
 <template>
