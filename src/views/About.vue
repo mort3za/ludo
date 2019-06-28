2907a92e52ff7b5dbb57d16b7b440229e16b9480
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-06-28 16:45:22 +0430

    style menus, remove favicons

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="about container py-3">
-    <h1 class="h1">Ludo Game</h1>
-    <p>This is a free and open source software. The source code is available at <a href="https://github.com/mort3za/ludo">Github</a>. Please feel free to send pull requests or create an issue.</p>
-    <p>Made with ♥ by <a href="https://github.com/mort3za">Morteza Ziyae</a> in Tehran, Iran.</p>
+  <div class="page-text shadow-sm">
+    <div class="container py-3">
+      <h1 class="h1">Ludo Game</h1>
+      <p>This is a free and open source software. The source code is available at <a href="https://github.com/mort3za/ludo">Github</a>. Please feel free to send pull requests or create an issue.</p>
+      <p>Made with ♥ by <a href="https://github.com/mort3za">Morteza Ziyae</a> in Tehran, Iran.</p>
 
-    <router-link class="btn btn-primary" to="/">Back</router-link>
+      <router-link class="btn btn-primary" to="/">Back</router-link>
+    </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="wrapper d-flex justify-content-center align-items-center overflow-auto">
     <div class="inner w-100 rounded mx-auto d-flex h-75 justify-content-center align-items-center">
       <div class="d-flex flex-column justify-content-center align-items-center p-3 w-100">
-        <h1 class="h1 title d-flex align-items-center"><img class="logo d-block mr-2" src="../assets/img/ludo.svg" alt="ludo"><span>Ludo Game</span></h1>
+        <h1 class="h1 title d-flex align-items-center font-weight-light">
+          <span>Ludo Game</span></h1>
         <div class="nav d-flex flex-column justify-content-center align-items-center w-100 mt-4">
           <router-link class="btn btn-lg w-75 btn-primary mb-3 font-weight-bolder rounded-pill shadow-sm" to="/play">Play</router-link>
           <router-link class="btn btn-lg w-75 btn-secondary mb-3 font-weight-normal rounded-pill shadow-sm" to="/about">About</router-link>
@@ -21,19 +22,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$logo-width: rem(48px);
+$logo-width: 48px;
 .wrapper {
   max-width: rem($board-width);
   height: 100vh;
   margin: auto;
 }
 .inner {
-  background: rgba(white, 0.8);
   min-height: rem(300px);
 }
 .title {
-  margin-left: rem(-$logo-width);
-  color: $dark-less;
+  color: $dark;
 }
 .logo {
   width: rem($logo-width);
