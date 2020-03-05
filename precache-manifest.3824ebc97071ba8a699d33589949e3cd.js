26bcd065732310ce68c5c69cc77e1b89f5416ad3
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-03-05 23:18:12 +0330

    deploy

---
+++ precache-manifest.3824ebc97071ba8a699d33589949e3cd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e78da9b0b8301dbbd2",
+    "url": "/ludo/css/about.b66098e1.css"
+  },
+  {
+    "revision": "6b77831fe3164771f099",
+    "url": "/ludo/css/app.6a3028c0.css"
+  },
+  {
+    "revision": "df9a272e8e094acdc696754c1c46145f",
+    "url": "/ludo/img/arrow.df9a272e.svg"
+  },
+  {
+    "revision": "ace67dfd37815293b5ac5424f1d3bf60",
+    "url": "/ludo/img/bg-diamond.ace67dfd.svg"
+  },
+  {
+    "revision": "be44764c560318d10babd1cb31edd885",
+    "url": "/ludo/img/bg-mountains.be44764c.png"
+  },
+  {
+    "revision": "31e2b8fa635de8f02c855c34f8d34466",
+    "url": "/ludo/img/close.31e2b8fa.svg"
+  },
+  {
+    "revision": "f87af671b2d4bde8a7f2517beac7f489",
+    "url": "/ludo/img/dice-1.f87af671.svg"
+  },
+  {
+    "revision": "8960316017cc9531ccdcbb430e45d6cf",
+    "url": "/ludo/img/dice-2.89603160.svg"
+  },
+  {
+    "revision": "ff70e02a8eebc259182352d713956da9",
+    "url": "/ludo/img/dice-3.ff70e02a.svg"
+  },
+  {
+    "revision": "ae99d3f2e742f301ebca44b5962c261f",
+    "url": "/ludo/img/dice-4.ae99d3f2.svg"
+  },
+  {
+    "revision": "9386670c13f93985d1f1f86b63370dc1",
+    "url": "/ludo/img/dice-5.9386670c.svg"
+  },
+  {
+    "revision": "04bb283ee6415ba46ee4da7476c77949",
+    "url": "/ludo/img/dice-6.04bb283e.svg"
+  },
+  {
+    "revision": "c5899f057958db97b29e0bd29bd149c5",
+    "url": "/ludo/img/flower.c5899f05.svg"
+  },
+  {
+    "revision": "73e06ca8a6493b1f8d75be94d1aa210a",
+    "url": "/ludo/img/menu.73e06ca8.svg"
+  },
+  {
+    "revision": "e51676431aa0bfbe29c989f166c37178",
+    "url": "/ludo/img/poster.png"
+  },
+  {
+    "revision": "95ae9a162ce83d13bc3626c2375283a9",
+    "url": "/ludo/img/star-side-1.95ae9a16.svg"
+  },
+  {
+    "revision": "af785de9b0ed1b4cdae51251977f54db",
+    "url": "/ludo/img/star-side-2.af785de9.svg"
+  },
+  {
+    "revision": "ffd57e04d79d3793a15214536c90f2f0",
+    "url": "/ludo/img/star-side-3.ffd57e04.svg"
+  },
+  {
+    "revision": "c74b08c4ff78a8068ac94246dd8fa3e4",
+    "url": "/ludo/img/star-side-4.c74b08c4.svg"
+  },
+  {
+    "revision": "c0964b35259df2aebbfb2d65912e90e3",
+    "url": "/ludo/img/star.c0964b35.svg"
+  },
+  {
+    "revision": "a9bcf0b6aa3c0a1eb86614d8997765de",
+    "url": "/ludo/img/turn-dice.a9bcf0b6.svg"
+  },
+  {
+    "revision": "23ee665210f1431837acb7b20a4c1b4e",
+    "url": "/ludo/index.html"
+  },
+  {
+    "revision": "f9e78da9b0b8301dbbd2",
+    "url": "/ludo/js/about.bf66260e.js"
+  },
+  {
+    "revision": "6b77831fe3164771f099",
+    "url": "/ludo/js/app.1b693f6e.js"
+  },
+  {
+    "revision": "ddbb4bcda2fdabe82822",
+    "url": "/ludo/js/chunk-vendors.034be83b.js"
+  },
+  {
+    "revision": "42fa3c317aac67352c26d45f53c8bd25",
+    "url": "/ludo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ludo/robots.txt"
+  }
+]);
